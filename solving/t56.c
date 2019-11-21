3a0d6cf2eb113da4b85262f1fccd2f99f5a03907
胡文栋 <huwendong@tuzhanai.com>
2019-11-21 19:34:44 +0800

    ppppp

---
+++ solving/t56.c
@@ -0,0 +1,32 @@
+//给出一个区间的集合，请合并所有重叠的区间。
+//
+// 示例 1:
+//
+// 输入: [[1,3],[2,6],[8,10],[15,18]]
+//输出: [[1,6],[8,10],[15,18]]
+//解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+//
+//
+// 示例 2:
+//
+// 输入: [[1,4],[4,5]]
+//输出: [[1,5]]
+//解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+// Related Topics 排序 数组
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** merge(int** intervals, int intervalsSize, int* intervalsColSize, int* returnSize, int** returnColumnSizes){
+
+}
+
+
+//leetcode submit region end(Prohibit modification and deletion)
